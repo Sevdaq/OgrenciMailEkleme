2ea33ee1e71eea26429a3e25ee5dc373bb09948c
Sevdaq <sevdaulukurt2@gmail.com>
2024-03-01 18:52:48 +0300

    Add project files.

---
+++ Ogrenci.cs
@@ -0,0 +1,40 @@
+﻿namespace OgrenciMail
+{
+    public class Ogrenci
+    {
+        public static string domainAdres;
+
+        public string Isim { get; set; }
+        public string Soyİsim { get; set; }
+
+        private string _emailadres;
+        public string Emailadres
+        {
+
+            get
+            {
+                return this._emailadres;
+            }
+            set
+            {
+
+                this._emailadres = value.ToLower() + "@" + Ogrenci.domainAdres;
+
+            }
+        }
+        public int cinsiyet { get; set; }
+
+        public Ogrenci()
+        {
+            Console.WriteLine("her örneklemede çalışan metod");
+
+        }
+        static Ogrenci()
+        {
+            domainAdres = "okulum.com";
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using OgrenciMail;
+
+namespace ConsoleApp48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci O1 = new Ogrenci();
+            O1.Isim = "Sevda";
+            O1.Soyİsim = "Galandarli";
+            O1.cinsiyet = 717770002;
+            O1.Emailadres = "sevda.galandarli";
+
+            Ogrenci O2 = new Ogrenci();
+            O2.Isim = "Arzu";
+            O2.Soyİsim = "Galandarli";
+            O2.cinsiyet = 717770001;
+            O2.Emailadres = "arzu.galandarli";
+
+        }
+    }
+}
